01834f520c8def3e08f53e0f97aeb412245cdf41
Louis <louis.mackenziesmith@googlemail.com>
2019-01-25 18:48:34 +0100

    second commit - game playable

--- countdown/numbersRound.py
+++ countdown/numbersRound.py
@@ -3,6 +3,18 @@ from the game countdown: receive 6 ints and a target int.
 it must be reached with + * / -
 '''
 
+import random
+import time
+
+below_zeros_allowed = False
+game_time_limit = 30
+
+
+class Stopper:
+    def __init__(self, int):
+        self.stop_at = int
+
+
 class SumTree:
     plus = "+"
     minus = "-"
@@ -15,8 +27,7 @@ class SumTree:
     operators.append(div)
 
     def __init__(self, root_value, remaining_array=[], target=None,
-                 number_node=True, parent=None, distance=0):
-
+                 number_node=True, parent=None, distance=0, stopper=None):
         self.parent = parent
         self.val = str(root_value)
         self.remainingArray = remaining_array
@@ -24,6 +35,7 @@ class SumTree:
         self.children = []
         self.distance = distance
         self.target = target
+        self.stopper = stopper
 
     def __str__(self):
         string = (self.distance * ". ") + str(self.val)
@@ -36,29 +48,41 @@ class SumTree:
     def addChild(self, val):
         self.children.append(val)
 
-    def populateTree(self):
+    def populate_tree(self):
+        global below_zeros_allowed
+        if self.target is None:
+            self.stopper.stop_at = self.stopper.stop_at - 1
+
         if len(self.remainingArray) == 0 and self.numberNode:
-            self.result = self.getResult()
+            self.result = self.get_result()
             self.children = [self.result]
 
             if self.target is not None:
-                if int(self.result) == self.target:
-                    # print("VICTORYYY")
-                    # print("target: " + str(self.target) + ", res: " + str(self.result))
-                    return self.getResultString()
+                if float(self.result).is_integer() and int(self.result) == self.target:
+                    return self.get_result_string()
+
+            else:
+                if self.stopper.stop_at <= 0 and float(self.result).is_integer():
+                    if below_zeros_allowed:
+                        return self.result, self.get_result_string()
+                    elif self.result >= 0:
+                        return self.result, self.get_result_string()
 
         elif self.numberNode:
             for operator in self.operators:
-                self.addChild(SumTree(operator, self.remainingArray, number_node=False, target=self.target, parent=self, distance=self.distance + 1))
+                self.addChild(SumTree(operator, self.remainingArray, number_node=False, target=self.target, parent=self,
+                                      distance=self.distance + 1, stopper=self.stopper))
 
         else:
             for remainer in self.remainingArray:
                 remainers = self.remainingArray.copy()
                 remainers.remove(remainer)
-                self.addChild(SumTree(remainer, remainers, target=self.target, parent=self, distance=self.distance + 1))
+
+                self.addChild(SumTree(remainer, remainers, target=self.target, parent=self, distance=self.distance + 1,
+                                      stopper=self.stopper))
 
     def recursivePop(self):
-        result = self.populateTree()
+        result = self.populate_tree()
         if result is not None:
             return result
         if len(self.remainingArray) > 0:
@@ -67,10 +91,10 @@ class SumTree:
                 if result is not None:
                     return result
 
-    def getResult(self):
-        return eval(self.getResultString())
+    def get_result(self):
+        return eval(self.get_result_string())
 
-    def getResultString(self):
+    def get_result_string(self):
         node = self
         a = []
         while node.parent is not None:
@@ -96,24 +120,105 @@ class SumTree:
         s = "".join(x for x in a)
         s = "".join(x for x in starter_brackets) + s
 
-        # print(s)
-
         return s
 
 
-# arr = [100, 1, 2, 3, 4, 5]
-arr = [100, 1, 2, 3, 4, 5, 6]
-objective = 102
+def find_permutation(arr, objective=None):
+    answer = None
+    tree = None
 
-# print("**************************")
-tree = SumTree(root_value=arr[0], remaining_array=arr[1:], target=objective)
-answer = tree.recursivePop()
-# print("**************************")
-# print(tree)
+    if objective is None:
+        first_entry = random.randint(0, len(arr) - 1)
+        stop_at = Stopper(random.randint(0, 200000))
 
-if answer is not None:
-    print("********************************")
-    print("solution found: ")
-    print(answer + " = " + str(tree.target))
-else:
-    print("No combination of all inputs %a found that results in %d" % (arr, objective))
+    for i in range(len(arr)):
+        remaining_array = []
+        remaining_array.extend(arr[:i])
+        remaining_array.extend(arr[i + 1:])
+
+        if objective is None:
+            if i != first_entry:
+                continue
+            tree = SumTree(root_value=arr[i], remaining_array=remaining_array, stopper=stop_at)
+
+        else:
+            tree = SumTree(root_value=arr[i], remaining_array=remaining_array, target=objective)
+
+        answer = tree.recursivePop()
+
+        if answer is not None:
+            break
+
+    if objective is None and tree is not None:
+        return answer
+
+    if answer is not None and tree is not None:
+        print("********************************")
+        print("solution found: ")
+        print(answer + " = " + str(tree.target))
+    else:
+        print("No combination of all inputs %a found that results in %d" % (arr, objective))
+
+    return answer
+
+
+def random_big():
+    return random.randint(2, 10) * 10
+
+
+def random_small():
+    return random.randint(1, 9)
+
+
+def get_random_numbers(b):
+    total = 6
+    s = total - b
+
+    smalls = []
+    for i in range(s):
+        smalls.append(random_small())
+
+    bigs = []
+    for i in range(b):
+        bigs.append(random_big())
+
+    all_randoms = []
+    all_randoms.extend(bigs)
+    all_randoms.extend(smalls)
+    return all_randoms
+
+
+def main():
+    number_of_bigs = 2
+    arr = get_random_numbers(number_of_bigs)
+    print("array is " + str(arr))
+
+    answer = find_permutation(arr)
+    print("score to find: " + str(answer[0]))
+
+    time.sleep(2)
+
+    print("you have %d seconds!" % game_time_limit)
+
+    for i in range(game_time_limit, 0, -1):
+        print("%d seconds left" % i)
+        time.sleep(1)
+
+    print("time's up!")
+    time.sleep(2)
+    print("did you get it?")
+    time.sleep(2)
+    print("the answer was of course " + str(answer[1]) + " = " + str(answer[0]) + " !")
+
+    time.sleep(2)
+
+    a = input("Do you want to play again (with different numbers)? [Y/n]\n")
+
+    if a == 'y' or a == 'y':
+        main()
+    else:
+        print("bye bye!")
+        exit(0)
+
+
+main()
